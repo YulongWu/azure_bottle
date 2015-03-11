4511872b0739f5283208bfa99e39ed23ef1decf2
Frankie Wu <wylong.0221@gmail.com>
2015-03-11 14:53:47 +0800

    load css file for bottle

--- main.py
+++ main.py
@@ -3,5 +3,11 @@ from bottle import route, static_file, run
 @route('/<filename:path>')
 def send_static(filename):
     return static_file(filename, root='./')
+@route('/')
+def main_static():
+    return static_file('index.html', root='./')
+@get('/<filename:re:.*\.css>')
+def stylesheets(filename):
+    return static_file(filename, root='./')
 
 run(host='localhost', port=8080, debug=True)
